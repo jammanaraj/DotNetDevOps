6a645940c3f2f219da8d1e63c99c282e0a6e95e4
Poul Kjeldager Sørensen <pks@s-innovations.net>
2019-05-14 23:13:58 +0200

    always run wiht usedev expr

--- src/DotNetDevOps.Web/Startup.cs
+++ src/DotNetDevOps.Web/Startup.cs
@@ -395,9 +395,11 @@ namespace DotNetDevOps.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+               
             }
 
             app.Map("/oidc-signin", (appinner) =>
