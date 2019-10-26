fbf8825d4776ae92592ed267e994412eba1216cd
Poul Kjeldager Sørensen <pks@delegate.dk>
2019-10-26 15:56:30 +0200

    added secret template

--- src/DotNetDevOps.Web/Controllers/DotNetDevOpsResourceProviderController.cs
+++ src/DotNetDevOps.Web/Controllers/DotNetDevOpsResourceProviderController.cs
@@ -533,6 +533,28 @@ namespace DotNetDevOps.Web
             return Ok(template);
         }
 
+        [HttpGet("providers/DotNetDevOps.AzureTemplates/templates/KeyVaults/{keyVaultName}/secrets/{secretName}")]
+        public async Task<IActionResult> AddSecretTemplate([FromServices] IOptions<EndpointOptions> endpoints, string keyVaultName, string secretName)
+        {
+            var template = await LoadTemplateAsync(endpoints.Value, "KeyVault.secret.json");
+
+            if (!string.IsNullOrEmpty(keyVaultName))
+            {
+                template.SelectToken("$.parameters.keyVaultName")["defaultValue"] = keyVaultName;
+            }
+            if (!string.IsNullOrEmpty(secretName))
+            {
+                template.SelectToken("$.parameters.secretName")["defaultValue"] = secretName;
+            }
+            template.SelectToken("$.parameters.certificateThumbprint").Parent.Remove();
+            template.SelectToken("$.outputs.certificateThumbprint").Parent.Remove();
+            template.SelectToken("$.outputs")["secretUriWithVersion"] = template.SelectToken("$.outputs.certificateUrlValue").DeepClone();
+            template.SelectToken("$.outputs.certificateUrlValue").Parent.Remove();
+            template.SelectToken("$.resources[0].properties.contentType").Parent.Remove();
+
+            return Ok(template);
+        }
+
         [HttpGet("providers/DotNetDevOps.AzureTemplates/templates/ServiceBus/namespaces/demo")]
         public async Task<IActionResult> GetServiceBusTempalte([FromServices] IOptions<EndpointOptions> endpoints, int topicSCaleCount = 2, params string[] correlationMappings)
         {
